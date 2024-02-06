d4aebc43aeefced7be5882493748ba21f7be7637
tahadurdu <tahadurdu@hotmail.com>
2024-02-06 13:29:05 +0300

    first commit

---
+++ Intro/Intro/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+using Intro.Business;
+using Intro.Entities;
+
+CourseManager courseManager = new();
+Course[] courses2 = courseManager.GetAll();
+
+foreach (var i in courses2)
+{
+    Console.WriteLine(i.Name);
+}
